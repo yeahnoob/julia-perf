e971f6d1f9d99415591c64874f724bbe2edca686
Xanadu Zhang <yeahnoob@buaa.edu.cn>
2014-10-01 10:57:48 +0800

    Remove dummy file test.

---
+++ count_pairs-1.jl
@@ -0,0 +1,48 @@
+function groupby{T} (fn, seq::Array{T})
+    dict = Dict{Any, Array{T}}()
+    #set the initial-size of "dict", for better running time performance.
+    sizehint(dict, length(seq) >> 5)
+    for item in seq
+        key = fn(item)
+        if !haskey(dict, key)
+            dict[key] = []
+        end
+        push!(dict[key], item)
+    end
+    return dict
+end
+
+function processdata(filename::String)
+    file = open(filename, "r")
+    try
+        lines = String[]
+        lines = readlines(file)
+        #maplines = map(strip, lines)
+        #println(length(maplines))
+        word_pairs = String[]
+        word_pairs = map(s->split(strip(s), ','), lines)
+        #println(length(word_pairs))
+        result = groupby(a->a[1], word_pairs)
+        return result
+    finally
+        close(file)
+    end
+end
+
+println("... Process \"word-paris.txt\" ", int(ARGS[1]), " Times ...")
+
+for i = 1:int(ARGS[1])
+    try
+        # wait for sevaral seconds, take easy.:) bypass Hardisk I/O speed difference
+        y = 0
+        for i = 1:10^6
+            y += i
+        end
+    finally
+        @time begin
+            processdata("word-pairs.txt")
+        end
+    end
+end
+
+None 
